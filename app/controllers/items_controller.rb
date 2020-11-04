3c96f54dcc2ee9f370ef05232135e375042833e3
atsuya-nakada <72425497+atsuya-nakada@users.noreply.github.com>
2020-11-04 16:35:35 +0900

    追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new ]
+  before_action :authenticate_user!, only: [:create, :new, :edit ]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -26,8 +26,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
